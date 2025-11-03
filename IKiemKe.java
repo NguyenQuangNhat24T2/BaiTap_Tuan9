5a0bfc5c429084a1857a191701317c02d29d8fb6
duin333 <duyennguyenthaiky@gmail.com>
2025-11-03 22:37:05 +0700

    Update IKiemKe.java

--- IKiemKe.java
+++ IKiemKe.java
@@ -1,8 +1,9 @@
 /**Giao dien Interface (Can-Do relationship)**/
-package BaiTap_Tuan8;
+package BaiTap_Tuan9;
 public interface IKiemKe
 {
     boolean kiemTraTonKho(int soLuongToiThieu);
     /*true (nghĩa là "Đúng" / "Có" / "Bật") / false (nghĩa là "Sai" / "Không" / "Tắt") */
     void capNhatViTri(String viTriMoi);
 }
+
